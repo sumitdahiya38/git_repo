6fb7ed41c94eb335c4e9309fd271fcc9382458a3
sumitdahiya38 <sumitdahiya38@gmail.com>
2018-02-09 13:16:24 +0530

    first change

---
+++ weblogic.py
@@ -0,0 +1,2 @@
+connect("weblogic","weblogic123","t3://172.17.17.115:7001")
+sca_deployComposite("http://172.17.17.115:7004","https://github.com/sumitdahiya38/SOA_Composite/blob/master/MyFirstApp/deploy/sca_MyFirstApp1.jar",user="weblogic",password="weblogic123",overwrite=true)
